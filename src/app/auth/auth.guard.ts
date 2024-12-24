4111ab733c3045e0303658ba932ab0e91d260e14
Mustafa <mustafajw07@gmail.com>
2024-12-24 17:09:22 +0530

    format

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../shared/services/auth.service';
